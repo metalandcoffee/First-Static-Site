14c0c37556092ffd780788dabd2844b3e18f30ea
Ebonie Butler <metalandcoffee@Ebonies-MacBook-Air.local>
2017-02-05 20:24:35 -0500

    Complete radio.html // Close Issue#2

--- assets/css-js/js/main.js
+++ assets/css-js/js/main.js
@@ -1,8 +1,9 @@
-//$(function () {
-//    var i = 0;
-//    $('table thead tr').prepend('<th>#</th>');
-//    $('table tbody tr').each(function () {
-//        i += 1;
-//        $(this).prepend('<td>' + i + '</td>');
-//    });
-//});
+$(function () {     
+    var i = 0;
+    $('table thead tr.numbered').prepend('<th>#</th>');
+    $('table tbody tr.numbered').each(function () {
+        i += 1;
+        $(this).prepend('<td>' + i + '</td>');
+    });
+
+});
